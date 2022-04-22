5155d40ffecdd6b86a505a742115c0b9d69ad1f0
piebin <76805688+piebin@users.noreply.github.com>
2022-04-22 23:49:17 +0900

    finish

--- app/src/main/java/com/example/kotlin_mind_test/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_mind_test/MainActivity.kt
@@ -2,16 +2,34 @@ package com.example.kotlin_mind_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.viewbinding.ViewBinding
 import com.example.kotlin_mind_test.databinding.ActivityMainBinding
+import com.example.kotlin_mind_test.databinding.FragmentMainBinding
 
 private lateinit var binding: ActivityMainBinding
 
-
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
+                as NavHostFragment
+        val navController = navHostFragment.navController
+
+
+
+
     }
+
 }
+
+
+
---
+++ app/src/main/java/com/example/kotlin_mind_test/fragment/SelectionFragment.kt
@@ -0,0 +1,104 @@
+package com.example.kotlin_mind_test.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.kotlin_mind_test.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SelectionFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SelectionFragment : Fragment() {
+
+
+
+    lateinit var navController: NavController
+    private var _binding: FragmentBlankBinding? = null//왤까...
+    private val binding get() = _binding!!
+
+
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_selection, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+
+        binding.btn_back.setOnClickListener(this)
+        binding.option1.setOnClickListener(this)
+        binding.option2.setOnClickListener(this)
+        binding.option3.setOnClickListener(this)
+        binding.option4.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v:View?){
+        when(v?.id){
+            R.id.option1->{navigateWithindex(1)}
+            R.id.option2->{navigateWithindex(2)}
+            R.id.option3->{navigateWithindex(3)}
+            R.id.option4->{navigateWithindex(4)}
+            R.id.btn_back->{
+                navController.popBackStack()
+            }
+        }
+    }
+
+    fun navigateWithindex(index:Int){
+        val bundle= bundleOf("index" to index)
+
+        navController.navigate(R.id.action_selectionFragment_to_resultFragment, bundle)
+    }
+
+
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment SelectionFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            SelectionFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
